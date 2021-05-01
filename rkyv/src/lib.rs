23c322befde86f23831bfb4391b37a77915a991b
David Koloski <djkoloski@gmail.com>
2021-05-01 00:36:46 -0400

    Add logo to docs

--- rkyv/src/lib.rs
+++ rkyv/src/lib.rs
@@ -66,6 +66,17 @@
 #![cfg_attr(feature = "specialization", feature(min_specialization))]
 #![cfg_attr(feature = "specialization", feature(rustc_attrs))]
 
+#![doc(html_favicon_url = r#"
+    data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg'
+    viewBox='0 0 26.458 26.458'%3E%3Cpath d='M0 0v26.458h26.458V0zm9.175 3.772l8.107 8.106
+    2.702-2.702 2.702 13.512-13.512-2.702 2.703-2.702-8.107-8.107z'/%3E%3C/svg%3E
+"#)]
+#![doc(html_logo_url = r#"
+    data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" width="100" height="100"
+    viewBox="0 0 26.458 26.458"%3E%3Cpath d="M0 0v26.458h26.458V0zm9.175 3.772l8.107 8.106
+    2.702-2.702 2.702 13.512-13.512-2.702 2.703-2.702-8.107-8.107z"/%3E%3C/svg%3E
+"#)]
+
 pub mod core_impl;
 pub mod de;
 pub mod ser;
