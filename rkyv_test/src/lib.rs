df6e05e392f94f023f628dc37e8099cd1dc948c6
b8591340 <70052269+b8591340@users.noreply.github.com>
2021-09-01 03:01:24 +0200

    Add optional support for granular endianness

--- rkyv_test/src/lib.rs
+++ rkyv_test/src/lib.rs
@@ -48,6 +48,36 @@ mod tests {
         test_archive(&Result::<i32, u32>::Ok(12345i32));
         test_archive(&Result::<i32, u32>::Err(12345u32));
         test_archive(&Some(42));
+
+        #[cfg(feature = "rkyv/rend")]
+        {
+            use rkyv::rend::*;
+            test_archive(f32_be::new(&1234567f32));
+            test_archive(f64_be::new(&12345678901234f64));
+            test_archive(i8_be::new(&123i8));
+            test_archive(i16_be::new(&12345i16));
+            test_archive(i32_be::new(&1234567890i32));
+            test_archive(i64_be::new(&1234567890123456789i64));
+            test_archive(i128_be::new(&123456789012345678901234567890123456789i128));
+            test_archive(u8_be::new(&123u8));
+            test_archive(u16_be::new(&12345u16));
+            test_archive(u32_be::new(&1234567890u32));
+            test_archive(u64_be::new(&12345678901234567890u64));
+            test_archive(u128_be::new(&123456789012345678901234567890123456789u128));
+
+            test_archive(f32_le::new(&1234567f32));
+            test_archive(f64_le::new(&12345678901234f64));
+            test_archive(i8_le::new(&123i8));
+            test_archive(i16_le::new(&12345i16));
+            test_archive(i32_le::new(&1234567890i32));
+            test_archive(i64_le::new(&1234567890123456789i64));
+            test_archive(i128_le::new(&123456789012345678901234567890123456789i128));
+            test_archive(u8_le::new(&123u8));
+            test_archive(u16_le::new(&12345u16));
+            test_archive(u32_le::new(&1234567890u32));
+            test_archive(u64_le::new(&12345678901234567890u64));
+            test_archive(u128_le::new(&123456789012345678901234567890123456789u128));
+        }
     }
 
     #[test]
@@ -97,6 +127,48 @@ mod tests {
             test_archive(&NonZeroU128::new_unchecked(
                 123456789012345678901234567890123456789,
             ));
+        }
+
+        #[cfg(feature = "rkyv/rend")]
+        unsafe {
+            use rkyv::rend::*;
+            test_archive(&NonZeroI8_be::new(NonZeroI8::new_unchecked(123)));
+            test_archive(&NonZeroI16_be::new(NonZeroI16::new_unchecked(12345)));
+            test_archive(&NonZeroI32_be::new(NonZeroI32::new_unchecked(1234567890)));
+            test_archive(&NonZeroI64_be::new(NonZeroI64::new_unchecked(
+                1234567890123456789,
+            )));
+            test_archive(&NonZeroI128_be::new(NonZeroI128::new_unchecked(
+                123456789012345678901234567890123456789,
+            )));
+            test_archive(&NonZeroU8_be::new(NonZeroU8::new_unchecked(123)));
+            test_archive(&NonZeroU16_be::new(NonZeroU16::new_unchecked(12345)));
+            test_archive(&NonZeroU32_be::new(NonZeroU32::new_unchecked(1234567890)));
+            test_archive(&NonZeroU64_be::new(NonZeroU64::new_unchecked(
+                1234567890123456789,
+            )));
+            test_archive(&NonZeroU128_be::new(NonZeroU128::new_unchecked(
+                123456789012345678901234567890123456789,
+            )));
+
+            test_archive(&NonZeroI8_le::new(NonZeroI8::new_unchecked(123)));
+            test_archive(&NonZeroI16_le::new(NonZeroI16::new_unchecked(12345)));
+            test_archive(&NonZeroI32_le::new(NonZeroI32::new_unchecked(1234567890)));
+            test_archive(&NonZeroI64_le::new(NonZeroI64::new_unchecked(
+                1234567890123456789,
+            )));
+            test_archive(&NonZeroI128_le::new(NonZeroI128::new_unchecked(
+                123456789012345678901234567890123456789,
+            )));
+            test_archive(&NonZeroU8_le::new(NonZeroU8::new_unchecked(123)));
+            test_archive(&NonZeroU16_le::new(NonZeroU16::new_unchecked(12345)));
+            test_archive(&NonZeroU32_le::new(NonZeroU32::new_unchecked(1234567890)));
+            test_archive(&NonZeroU64_le::new(NonZeroU64::new_unchecked(
+                1234567890123456789,
+            )));
+            test_archive(&NonZeroU128_le::new(NonZeroU128::new_unchecked(
+                123456789012345678901234567890123456789,
+            )));
         }
     }
 
