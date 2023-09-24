0a58dc54f4ece07048c07df359678ba29e1f3101
Ivan Boldyrev <ivan@fluence.one>
2023-09-24 17:35:08 +0400

    Use `Entry` API to entry double hash lookup
    In the `rkyv::validation::validators::shared::SharedValidator`.  It
    makes validation of some sample data with lot of shared values faster by
    about 1%.

--- rkyv/src/validation/validators/shared.rs
+++ rkyv/src/validation/validators/shared.rs
@@ -89,18 +89,24 @@ impl SharedContext for SharedValidator {
         ptr: *const u8,
         type_id: TypeId,
     ) -> Result<bool, Self::Error> {
-        if let Some(previous_type_id) = self.shared.get(&ptr) {
-            if previous_type_id != &type_id {
-                Err(SharedError::TypeMismatch {
-                    previous: *previous_type_id,
-                    current: type_id,
-                })
-            } else {
-                Ok(false)
+        use std::collections::hash_map::Entry;
+
+        match self.shared.entry(ptr) {
+            Entry::Occupied(previous_type_entry) => {
+                let previous_type_id = previous_type_entry.get();
+                if previous_type_id != &type_id {
+                    Err(SharedError::TypeMismatch {
+                        previous: *previous_type_id,
+                        current: type_id,
+                    })
+                } else {
+                    Ok(false)
+                }
             }
-        } else {
-            self.shared.insert(ptr, type_id);
-            Ok(true)
+            Entry::Vacant(ent) => {
+                ent.insert(type_id);
+                Ok(true)
+            }
         }
     }
 }
