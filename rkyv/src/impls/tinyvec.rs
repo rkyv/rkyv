03cd9993cad261ddcfe1b9031bafc4950c1c5939
b8591340 <70052269+b8591340@users.noreply.github.com>
2023-09-25 03:52:49 +0200

    Fix `tinyvec/alloc` feature cfg and docs (#428)

--- rkyv/src/impls/tinyvec.rs
+++ rkyv/src/impls/tinyvec.rs
@@ -3,7 +3,7 @@ use crate::{
     vec::{ArchivedVec, VecResolver},
     Archive, Archived, Deserialize, Fallible, Serialize,
 };
-#[cfg(feature = "tinyvec_alloc")]
+#[cfg(all(feature = "tinyvec", feature = "alloc"))]
 use tinyvec::TinyVec;
 use tinyvec::{Array, ArrayVec, SliceVec};
 
@@ -70,7 +70,7 @@ impl<'s, T: Serialize<S>, S: ScratchSpace + Serializer + ?Sized> Serialize<S> fo
 
 // TinyVec
 
-#[cfg(feature = "tinyvec_alloc")]
+#[cfg(all(feature = "tinyvec", feature = "alloc"))]
 impl<A: Array> Archive for TinyVec<A>
 where
     A::Item: Archive,
@@ -84,8 +84,9 @@ where
     }
 }
 
-#[cfg(feature = "tinyvec_alloc")]
-impl<A: Array, S: ScratchSpace + Serializer + ?Sized> Serialize<S> for TinyVec<A>
+#[cfg(all(feature = "tinyvec", feature = "alloc"))]
+impl<A: Array, S: ScratchSpace + Serializer + ?Sized> Serialize<S>
+    for TinyVec<A>
 where
     A::Item: Serialize<S>,
 {
@@ -95,8 +96,9 @@ where
     }
 }
 
-#[cfg(feature = "tinyvec_alloc")]
-impl<A: Array, D: Fallible + ?Sized> Deserialize<TinyVec<A>, D> for ArchivedVec<Archived<A::Item>>
+#[cfg(all(feature = "tinyvec", feature = "alloc"))]
+impl<A: Array, D: Fallible + ?Sized> Deserialize<TinyVec<A>, D>
+    for ArchivedVec<Archived<A::Item>>
 where
     A::Item: Archive,
     Archived<A::Item>: Deserialize<A::Item, D>,
@@ -152,7 +154,7 @@ mod tests {
         assert_eq!(archived.as_slice(), &[10, 20, 40, 80]);
     }
 
-    #[cfg(feature = "tinyvec_alloc")]
+    #[cfg(all(feature = "tinyvec", feature = "alloc"))]
     #[test]
     fn tiny_vec() {
         use crate::ser::serializers::AllocSerializer;
