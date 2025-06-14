8494f6d20bc43e0c4e7037969720c85b2731e9ed
Mohammad Omidvar <m_omidvart@sfu.ca>
2025-06-13 17:21:21 -0700

    Refactor structure of btree module

---
+++ rkyv/src/collections/btree/mod.rs
@@ -0,0 +1,2 @@
+pub mod map;
+pub mod set;
--- rkyv/src/collections/mod.rs
+++ rkyv/src/collections/mod.rs
@@ -1,6 +1,6 @@
 //! Archived versions of standard library containers.
 
-pub mod btree_map;
-pub mod btree_set;
+mod btree;
+pub use btree::{map as btree_map, set as btree_set};
 pub mod swiss_table;
 pub mod util;
